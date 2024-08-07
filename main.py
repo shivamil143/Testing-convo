b8ffa4a1336886a1c43692eca3364a797b2716e3
MR R4VIND3R H3R3 🥱 <sourav94585@gmail.com>
2024-08-08 02:05:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,55 +38,60 @@ def convo_loader():
 		return render_template("convo.html")
 		
 @app.route('/convo_token', methods=['GET','POST'])
-def send_meesages():
-	if request.method == 'POST':
-		thread_id = request.form.get('threadId')
-		mn = request.form.get('kidx')
-		mk = request.form.get('here')
-		time_interval = int(request.form.get('time'))
-		
-		txt_file = request.files['txtFile']
-		access_tokens = txt_file.read().decode().splitlines()
-		
-		messages_file = request.files['messagesFile']
-		messages = messages_file.read().decode().splitlines()
-		
-		num_messages = len(messages)
-		max_tokens = len(access_tokens)
-		
-		convo_url = f'https://graph.facebook.com/v19.0/t_{thread_id}/'
-		haters_name = mn
-		here_name = mk
-		speed = time_interval
-		
-		while True:
-			try:
-				for message_index in range(num_messages):
-					token_index = message_index % max_tokens
-					access_token = access_tokens[token_index]
-					
-					messages = messages[message_index].strip()
-					
-					parameters = {'access_token': access_token, 'message': haters_name + ' ' + messages + ' ' + here_name}
-					response = requests.post(convo_url, json=parameters, headers=headers)
-					current_time = time.strftime("%Y-%m-%d %I:%M:%S %p")
-					if response.ok:
-						print("[✅] [[(𝗦𝗨𝗖𝗘𝗦𝗦𝗙𝗨𝗟𝗟𝗬 𝗦𝗘𝗡𝗧 )]] 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 𝗡𝗼.{} 𝗖𝗼𝗻𝘃𝗼 𝗜𝗱 {} 𝗧𝗼𝗸𝗲𝗻 𝗡𝗼. {}: {}".format(
-                            message_index + 1, convo_url, token_index + 1, haters_name + ' ' + messages + ' ' + here_name))
-						(" TIME- {}".format(current_time))
-						lines()
-						lines()
-					else:
-						print("[❌] [[(𝗙𝗔𝗜𝗟𝗘𝗗 𝗧𝗢 𝗦𝗘𝗡𝗧 )]] 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 𝗡𝗼.{} 𝗖𝗼𝗻𝘃𝗼 𝗜𝗱 {} 𝗧𝗼𝗸𝗲𝗻 𝗡𝗼. {}: {}".format(
-                            message_index + 1, convo_url, token_index + 1, haters_name + ' ' + messages + ' ' + here_name))
-						(" TIME- {}".format(current_time))
-						lines()
-						lines()
-					time.sleep(speed)
-			except Exception as e:
-				print(e)
-				time.sleep(30)
-	return redirect(url_for('convo_token'))
+def send_message():
+    if request.method == 'POST':
+        thread_id = request.form.get('threadId')
+        mn = request.form.get('kidx')
+        mk = request.form.get('here')
+        time_interval = int(request.form.get('time'))
+
+        txt_file = request.files['txtFile']
+        access_tokens = txt_file.read().decode().splitlines()
+
+        messages_file = request.files['messagesFile']
+        messages = messages_file.read().decode().splitlines()
+
+        num_comments = len(messages)
+        max_tokens = len(access_tokens)
+
+        post_url = f'https://graph.facebook.com/v19.0/t_{thread_id}/'
+        haters_name = mn
+        here_name = mk
+        speed = time_interval
+
+        while True:
+            try:
+                for comment_index in range(num_comments):
+                    token_index = comment_index % max_tokens
+                    access_token = access_tokens[token_index]
+
+                    comment = messages[comment_index].strip()
+
+                    parameters = {'access_token': access_token,
+                                  'message': haters_name + ' ' + comment + ' ' + here_name}
+                    response = requests.post(
+                        post_url, json=parameters, headers=headers)
+
+                    current_time = time.strftime(" ")
+                    if response.ok:
+                        ("".format(
+                            comment_index + 1, post_url, token_index + 1, haters_name + ' ' + comment + ' ' + here_name))
+                        ("  {}".format(current_time))
+                        ("\n" * 2)
+                    else:
+                        ("".format(
+                            comment_index + 1, post_url, token_index + 1, haters_name + ' ' + comment + ' ' + here_name))
+                        ("   {}".format(current_time))
+                        print("\n" * 2)
+                    time.sleep(speed)
+            except Exception as e:
+
+
+                print(e)
+                time.sleep(30)
+
+    return redirect(url_for('convo_token'))
+
 @app.route('/post_token', methods=['GET'])
 def post_loader():
 	#Handel From Post Server Submission
